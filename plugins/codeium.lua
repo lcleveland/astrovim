5e95974f86369db80aee0a9a7525c15c413ed74b
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-12-07 13:39:07 -0600

    set down as completion

--- plugins/codeium.lua
+++ plugins/codeium.lua
@@ -2,5 +2,8 @@ return {
   {
     "Exafunction/codeium.vim",
     event = 'BufEnter',
+    config = function()
+      vim.keymap.set('i', '<Down>', function() return vim.fn['codeium#Accept']() end, { expr = true})
+    end,
   },
 }
