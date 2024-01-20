640bfc327ae8dd5ef377d89c295a12bef3646eb7
Lyle Cleveland <lyle.cleveland@yahoo.com>
2024-01-20 15:17:59 -0600

    move to using community where possible

--- plugins/codeium.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "Exafunction/codeium.vim",
-    event = 'BufEnter',
-    config = function()
-      vim.keymap.set('i', '<Down>', function() return vim.fn['codeium#Accept']() end, { expr = true})
-    end,
-  },
-}
